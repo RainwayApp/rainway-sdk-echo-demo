c54eab78f5702d5a81869b972871aca2334d7f63
Andrew Sampson <andrew@rainway.com>
2021-06-24 22:35:56 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using Rainway.SDK;
+
+
+static string ReverseString(byte[] data)
+{
+    var str = Encoding.UTF8.GetString(data);
+    var charArray = str.ToCharArray();
+    Array.Reverse(charArray);
+    return new string(charArray);
+}
+
+// the runtime configuration
+var config = new RainwayConfig
+{
+    // your publishable API key should go here
+    ApiKey = string.Empty,
+    ExternalId = string.Empty,
+    LogSink = (level, message) => Console.WriteLine($"[RW][{level.ToString().ToUpper()}] {message}"),
+    // audo accepts all connection request
+    OnConnectionRequest = (request) => request.Accept(),
+    // auto accepts all stream request and gives full input privileges to the remote peer 
+    OnStreamRequest = (requests) => requests.Accept(new RainwayPeerPermissions(true, true, true)),
+    // reverses the data sent by a peer and echos it back
+    OnPeerMessage = (peer, data) => peer.Send(ReverseString(data))
+};
+
+// initalize the runtime
+using var runtime = await RainwayRuntime.Initialize(config);
+runtime.SetLogLevel(RainwayLogLevel.Information);
+Console.WriteLine($"Rainway SDK Version: {runtime.Version}");
+Console.WriteLine($"Hostname: {runtime.Hostname}");
+Console.WriteLine("Press Ctrl+C To Terminate");
+
+var closeEvent = new AutoResetEvent(false);
+
+Console.CancelKeyPress += (sender, eventArgs) =>
+{
+    eventArgs.Cancel = true;
+    closeEvent.Set();
+};
+
+closeEvent.WaitOne();
---
+++ README.md
@@ -0,0 +1,7 @@
+﻿# C# Console Demo
+
+This example project shows how to build a simple echo server using the Rainway SDK in a .NET 5 console application. 
+
+# Note
+
+The 0.1.0 version of the rainway-sdk nuget package does not properly copy the `RainwayRuntime.dll` and `DesktopCapture.dll` to the output directory. To resolve this select the files in the solution explorer and change "Copy to Output" to "Always"
